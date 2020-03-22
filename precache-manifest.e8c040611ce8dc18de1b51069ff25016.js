2dca38daf1bed114550fa2e9ce904ca9570aff5a
mahad <mahadamer99@gmail.com>
2020-03-22 22:33:34 +0100

    Updates

---
+++ precache-manifest.e8c040611ce8dc18de1b51069ff25016.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d1da425ee028dcccf0b041d8596d8a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3dc8e89824fa47ea1a4d",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "600672b2d3fb7033aef1",
+    "url": "/robofriends/static/css/main.2c6792b5.chunk.css"
+  },
+  {
+    "revision": "3dc8e89824fa47ea1a4d",
+    "url": "/robofriends/static/js/2.58272216.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.58272216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "600672b2d3fb7033aef1",
+    "url": "/robofriends/static/js/main.b032247a.chunk.js"
+  },
+  {
+    "revision": "6a5eb8d065c05feaff0d",
+    "url": "/robofriends/static/js/runtime-main.5530ba65.js"
+  }
+]);
